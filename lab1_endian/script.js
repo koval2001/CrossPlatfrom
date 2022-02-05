90d4ce5aca3e0d6c40af5e9679164649760aa4e0
koval2001 <d.koval@geniusee.com>
2022-02-05 18:11:16 +0200

    endian swap with library

--- lab1_endian/script.js
+++ lab1_endian/script.js
@@ -1,3 +1,7 @@
+const endianCode = require('endian-code');
+
+console.log("Library: ", endianCode.encode(0x11869F, 3, false).map(x => x.toString(16)));
+
 let a = 1148575;
 
 function decimalToHexString(number)
@@ -11,13 +15,13 @@ function decimalToHexString(number)
 }
 
 function hexToBytes(hex) {
-  for (let bytes = [], c = 0; c < hex.length; c += 2)
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
     bytes.push(parseInt(hex.substr(c, 2), 16));
   return bytes;
 }
 
 function bytesToHex(bytes) {
-  for (let hex = [], i = 0; i < bytes.length; i++) {
+  for (var hex = [], i = 0; i < bytes.length; i++) {
     let current = bytes[i] < 0 ? bytes[i] + 256 : bytes[i];
     hex.push((current >>> 4).toString(16));
     hex.push((current & 0xF).toString(16));

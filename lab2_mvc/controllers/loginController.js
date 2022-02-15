b14a8913be519bb829744fb5be136b5b7a999499
koval2001 <d.koval@geniusee.com>
2022-02-15 23:57:58 +0200

    initial work of mvc & connect to mongo db

---
+++ lab2_mvc/controllers/loginController.js
@@ -0,0 +1,62 @@
+//js
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+//js
+//Post Request that handles Register
+const registerUser = (req, res) => {
+  const { name, email, location, password, confirm } = req.body;
+  if (!name || !email || !password || !confirm) {
+    console.log("Fill empty fields");
+  }
+  //Confirm Passwords
+  if (password !== confirm) {
+    console.log("Password must match");
+  } else {
+    //Validation
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        console.log("email exists");
+        res.render("register", {
+          name,
+          email,
+          password,
+          confirm,
+        });
+      } else {
+        //Validation
+        const newUser = new User({
+          name,
+          email,
+          location,
+          password,
+        });
+        //Password Hashing
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(res.redirect("/login"))
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
+};
+//For Register Page
+const registerView = (req, res) => {
+  res.render("register", {
+  } );
+}
+// For View
+const loginView = (req, res) => {
+
+  res.render("login", {
+  } );
+}
+module.exports =  {
+  registerView,
+  loginView
+};
